95d35a22da0162cb1b6e443ed6b6c831afbf28ed
Adrian <kowalski.adrian.5@gmail.com>
2019-04-09 23:55:10 +0200

    Add login, register form validation. To do: Create user Eloquent & upload avatar

---
+++ public/src/app/auth/auth.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnDestroy, OnInit, Renderer2, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class AuthComponent implements OnInit, OnDestroy {
+
+  constructor(private renderer: Renderer2) {
+    this.renderer.addClass(document.body, 'text-center');
+  }
+
+  ngOnDestroy() {
+    this.renderer.removeClass(document.body, 'text-center');
+  }
+
+  ngOnInit() {
+  }
+
+}
