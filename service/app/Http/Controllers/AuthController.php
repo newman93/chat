8413af9638cddbda6d90fafef4a5b6f75aeab99d
Adrian <kowalski.adrian.5@gmail.com>
2019-04-14 16:01:52 +0200

    Fix backend

--- service/app/Http/Controllers/AuthController.php
+++ service/app/Http/Controllers/AuthController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
-use App\Messages;
 use App\User;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
